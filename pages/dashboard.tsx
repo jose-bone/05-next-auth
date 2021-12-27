f8ca9b58a1719e9d4dd82d5930778bc674a38eff
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-27 22:55:12 +0200

    Recuperando estado da autenticação

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,5 +3,10 @@ import { AuthContext } from "../contexts/AuthContext";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    api.get("/me").then((response) => console.log(response));
+  }, []);
+
   return <h1>Dashboard: {user?.email}</h1>;
 }
