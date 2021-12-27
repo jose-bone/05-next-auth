f28bc02648cc7898ebdf075ef90d5ebeb6c57a86
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-27 22:22:24 +0200

    Salvando tokens nos cookies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -35,6 +35,15 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
       const { token, refreshToken, permissions, roles } = response.data;
 
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+
       setUser({
         email,
         permissions,
